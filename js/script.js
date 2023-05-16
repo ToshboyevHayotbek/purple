77a57cd9ff7e5943c1ca6c463902f01f4e21b091
Your Name <you@example.com>
2023-05-16 14:53:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('DOMContentLoaded', () =>{
+
+})
+
+const loader = document.querySelector('.loader');
+
+setTimeout(() => {
+  loader.style.opacity = 0.5;
+}, 2800);
+setTimeout(() => {
+  loader.style.display = 'none';
+}, 3000)
+
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+const titleContent = document.querySelector('.title__content'),
+      blogItem = document.querySelectorAll('.blog__item'),
+      blogContainer = document.querySelectorAll('.blog__container'),
+      blogItemActive = document.querySelector('.blog__item__active');
+      
+      function hideBlogContainer (){
+        blogContainer.forEach(item  =>{
+          item.classList.add('hide');
+          item.classList.remove('show', '.grid');
+        });
+        blogItem.forEach(item =>{
+          item.classList.remove('blogItemActive');
+        });
+      }
+
+      function showBlogContainer (i = 0){
+        blogContainer[i].classList.add('show', '.grid');
+        blogContainer[i].classList.remove('hide');
+        blogItem[i].classList.add('blogItemActive');
+      }
+
+      hideBlogContainer();
+      showBlogContainer();
+
